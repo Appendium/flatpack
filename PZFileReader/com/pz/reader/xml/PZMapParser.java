73647f36b9ada99ead4f9d0f67e459369342ca74
zepernick <zepernick@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-01-27 12:05:35 +0000

    Enhancement changes for bug request # 1402993

--- PZFileReader/com/pz/reader/xml/PZMapParser.java
+++ PZFileReader/com/pz/reader/xml/PZMapParser.java
@@ -1,19 +1,19 @@
-
 /*
-Copyright 2005 Paul Zepernick
+ Copyright 2006 Paul Zepernick
 
-Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at 
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at 
 
-http://www.apache.org/licenses/LICENSE-2.0 
+ http://www.apache.org/licenses/LICENSE-2.0 
 
-Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.  
-*/
+ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.  
+ */
 /*
  * Created on Dec 31, 2004
  */
 package com.pz.reader.xml;
 
 import java.io.File;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,89 +22,97 @@ import org.jdom.Element;
 import org.jdom.input.SAXBuilder;
 
 import com.pz.reader.structure.ColumnMetaData;
+import com.pz.reader.util.ParserUtils;
 
 /**
  * @author zepernick
- *
+ * 
  * Parses a PZmap definition XML file
  */
 public class PZMapParser {
-    /**xml document to be parsed*/
-    private File XMLDocument = null;
-    /**collection of Column objects.  Names and lengths*/
-    private ArrayList columnDescriptors = new ArrayList();
-    
-    
-    /**
-     * Constructor
-     *
-     *@param XMLDocument - xml file to be parsed
-     */
-    public PZMapParser(File XMLDocument){
-        super();
-        this.XMLDocument = XMLDocument;
-    }
-    
-    
-    /**
-     * Reads the XMLDocument for a PZMap file
-     *
-     *@exception Exception
-     */
-    public void parse() throws Exception{
-        Document document = null;
-        SAXBuilder builder = null;
-        Element root = null;
-        List columns = null;
-        Element column = null;
-        ColumnMetaData columnObj = null;
-        
-        
+
+	/**
+	 * Constructor
+	 * 
+	 * @param XMLDocument -
+	 *            xml file to be parsed
+	 */
+	private PZMapParser() {
+	}
+
+	/**
+	 * Reads the XMLDocument for a PZMap file.
+	 * Parses the XML file, and returns a List of ColumnMetaData.
+	 * @param xmlFile XML file
+	 * @return List of ColumnMetaData
+	 * @throws Exception
+     * @deprecated
+	 */
+	public static List parse(File xmlFile) throws Exception {
+		List columnDescriptors = null;
+		InputStream xmlStream = ParserUtils.createInputStream(xmlFile);
+		columnDescriptors = parse(xmlStream);
+		if (columnDescriptors == null) {
+			columnDescriptors = new ArrayList();
+		}
+		return columnDescriptors;
+	}
+
+	/**
+     * TODO New method based on InputStream.
+	 * Reads the XMLDocument for a PZMap file from an InputStream, WebStart combatible.
+	 * Parses the XML file, and returns a List of ColumnMetaData.
+	 * @param xmlStream
+	 * @return
+	 * @throws Exception
+	 */
+	public static List parse(InputStream xmlStream) throws Exception {
+		ArrayList columnDescriptors = new ArrayList();
+		Document document = null;
+		SAXBuilder builder = null;
+		Element root = null;
+		List columns = null;
+		Element column = null;
+		ColumnMetaData columnObj = null;
+
 		builder = new SAXBuilder();
 		builder.setValidation(true);
-		document = builder.build(XMLDocument);
-		
-		
+		document = builder.build(xmlStream);
+
 		root = document.getRootElement();
 		columns = root.getChildren();
-		
-		for (int i = 0; i < columns.size(); i++){
-		    columnObj = new ColumnMetaData();
-		    column = (Element)columns.get(i);
-		    
-		    //make sure the name attribute is present on the column
-		    if (column.getAttributeValue("name") == null){
-		        throw new Exception("Name attribute is required on the column tag!");
-		    }
-		    
-		    columnObj.setColName(column.getAttributeValue("name"));
-		    
-		    //check to see if the column length can be set
-		    if (column.getAttributeValue("length") != null){
-		        try{
-		            columnObj.setColLength(Integer.parseInt(column.getAttributeValue("length")));
-		        }catch(Exception ex){
-		            throw new Exception("LENGTH ATTRIBUTE ON COLUMN ELEMENT MUST BE AN INTEGER.  GOT: " + column.getAttributeValue("length"));
-		        }
-		    }
-		    
-		  //  System.out.println("Column Name: " + column.getAttributeValue("name") + " LENGTH: " + column.getAttributeValue("length"));
-		    
-		    columnDescriptors.add(columnObj);
-		    
+
+		for (int i = 0; i < columns.size(); i++) {
+			columnObj = new ColumnMetaData();
+			column = (Element) columns.get(i);
+
+			// make sure the name attribute is present on the column
+			if (column.getAttributeValue("name") == null) {
+				throw new Exception(
+						"Name attribute is required on the column tag!");
+			}
+
+			columnObj.setColName(column.getAttributeValue("name"));
+
+			// check to see if the column length can be set
+			if (column.getAttributeValue("length") != null) {
+				try {
+					columnObj.setColLength(Integer.parseInt(column
+							.getAttributeValue("length")));
+				} catch (Exception ex) {
+					throw new Exception(
+							"LENGTH ATTRIBUTE ON COLUMN ELEMENT MUST BE AN INTEGER.  GOT: "
+									+ column.getAttributeValue("length"));
+				}
+			}
+
+			// System.out.println("Column Name: " +
+			// column.getAttributeValue("name") + " LENGTH: " +
+			// column.getAttributeValue("length"));
+
+			columnDescriptors.add(columnObj);
+
 		}
-		
-		
-		
-    }
-    
-    
-    /**
-     * Returns a list of column objects generated by the parse
-     * 
-     * @return Returns the columnDescriptors.
-     */
-    public ArrayList getColumnDescriptors() {
-        return columnDescriptors;
-    }
+		return columnDescriptors;
+	}
 }
