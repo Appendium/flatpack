f5599ce281baa3083aa304b014db2443fd6abd83
zepernick <zepernick@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-02-26 23:04:12 +0000

    *** empty log message ***

---
+++ com/pz/reader/Version.java
@@ -0,0 +1,14 @@
+/*
+ * Created on Feb 26, 2006
+ *
+*/
+package com.pz.reader;
+
+/**
+ * @author zepernick
+ *
+ * Static class which stores the version of this pzFileReader
+ */
+public class Version {
+    public static final String VERSION = "2.1.0";
+}
