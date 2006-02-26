e2fa3ad104be29d2a7e4e8ac026ffe0dac22ce04
zepernick <zepernick@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-02-26 22:58:44 +0000

    *** empty log message ***

---
+++ com/test/delim/columnInFile/TestDelimitedColumnNamesInFile.java
@@ -0,0 +1,75 @@
+/*
+ * Created on Feb 26, 2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.test.delim.columnInFile;
+
+import com.pz.reader.DataSet;
+
+import junit.framework.TestCase;
+
+/**
+ * @author zepernick
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TestDelimitedColumnNamesInFile extends TestCase {
+    public TestDelimitedColumnNamesInFile(
+            String name) {
+            super(name);
+        }
+    
+    	//tests to make sure we have 0 errors
+        public void testErrorCount() {
+            DataSet ds = null;
+            
+            try{
+                DelimitedColumnNamesInFile testDelimted = new DelimitedColumnNamesInFile();
+            
+                ds = testDelimted.getDsForTest();
+                              
+                //check that we had no errors
+                assertEquals(0, ds.getErrors().size());
+        
+            
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }finally{
+                if (ds != null) ds.freeMemory();
+            }
+        }
+        
+        //test to make sure we parsed the correct number
+        //of rows in the file
+        public void testRowCount(){
+            DataSet ds = null;
+            
+            try{
+                DelimitedColumnNamesInFile testDelimted = new DelimitedColumnNamesInFile();
+            
+                ds = testDelimted.getDsForTest();
+                
+                //check that we parsed in the right amount of rows
+                assertEquals(6, ds.rows.size());
+                              
+                //check that we had no errors
+                assertEquals(0, ds.getErrors().size());
+        
+            
+            }catch(Exception ex){
+                ex.printStackTrace();
+            }finally{
+                if (ds != null) ds.freeMemory();
+            }
+        }
+        
+        
+        
+        public static void main(String[] args) {
+            junit.textui.TestRunner.run(
+                TestDelimitedColumnNamesInFile.class);
+        }
+}
