5e83f7c8810a2e3485114aa627eb57b6c9b0a664
Paul Zepernick <zepernick@users.noreply.github.com>
2006-08-18 21:52:47 +0000

    *** empty log message ***

--- PZFileReader/com/pz/reader/Version.java
+++ PZFileReader/com/pz/reader/Version.java
@@ -9,5 +9,5 @@ package com.pz.reader;
  * Static class which stores the version of this pzFileReader
  */
 public class Version {
-    public static final String VERSION = "2.2.1";
+    public static final String VERSION = "2.2.2";
 }
