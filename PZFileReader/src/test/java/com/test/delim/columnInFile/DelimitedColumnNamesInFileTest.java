cce80a0d660b52fbada7585de5cd5731f4845672
benoitx <benoitx@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-10-16 13:44:42 +0000

    Added unit tests in pzfilereader (quite limited).
    Former-commit-id: a2fd61440afff63cb431a70ab79be361112414a1

--- PZFileReader/src/test/java/com/test/delim/columnInFile/DelimitedColumnNamesInFileTest.java
+++ PZFileReader/src/test/java/com/test/delim/columnInFile/DelimitedColumnNamesInFileTest.java
@@ -16,79 +16,79 @@ import junit.framework.TestCase;
  * TODO To change the template for this generated type comment go to
  * Window - Preferences - Java - Code Style - Code Templates
  */
-public class TestDelimitedColumnNamesInFile extends TestCase {
-    public TestDelimitedColumnNamesInFile(
+public class DelimitedColumnNamesInFileTest extends TestCase {
+    public DelimitedColumnNamesInFileTest(
             String name) {
             super(name);
         }
-    
+
     	//tests to make sure we have 0 errors
         public void testErrorCount() {
             DataSet ds = null;
-            
+
             try{
                 DelimitedColumnNamesInFile testDelimted = new DelimitedColumnNamesInFile();
-            
+
                 ds = testDelimted.getDsForTest();
-                              
+
                 //check that we had no errors
                 assertEquals(0, ds.getErrors().size());
-        
-            
+
+
             }catch(Exception ex){
                 ex.printStackTrace();
             }finally{
                 if (ds != null) ds.freeMemory();
             }
         }
-        
+
         //test to make sure we parsed the correct number
         //of rows in the file
         public void testRowCount(){
             DataSet ds = null;
-            
+
             try{
                 DelimitedColumnNamesInFile testDelimted = new DelimitedColumnNamesInFile();
-            
+
                 ds = testDelimted.getDsForTest();
-                
+
                 //check that we parsed in the right amount of rows
                 assertEquals(6, ds.rows.size());
-                              
-        
-            
+
+
+
             }catch(Exception ex){
                 ex.printStackTrace();
             }finally{
                 if (ds != null) ds.freeMemory();
             }
         }
-        
-        
+
+
         //test to make sure we have the right number of column names from the file
         public void testColumnNameCount(){
             DataSet ds = null;
-            
+
             try{
                 DelimitedColumnNamesInFile testDelimted = new DelimitedColumnNamesInFile();
-            
+
                 ds = testDelimted.getDsForTest();
-                
+
                 //check that we parsed in the right amount of column names
                 assertEquals(6, ds.getColumns().length);
-                              
-        
-            
+
+
+
             }catch(Exception ex){
                 ex.printStackTrace();
             }finally{
                 if (ds != null) ds.freeMemory();
             }
         }
-        
-        
+
+
         public static void main(String[] args) {
             junit.textui.TestRunner.run(
-                TestDelimitedColumnNamesInFile.class);
+                DelimitedColumnNamesInFileTest.class);
         }
 }
