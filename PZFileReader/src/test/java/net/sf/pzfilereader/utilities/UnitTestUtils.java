e60fc4c4a95ae467313d85b23444e3bb3f1e9d25
benoitx <benoitx@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-10-24 23:33:29 +0000

    Optimised the ParserUtils to use char for delimiter and qualifier. I have added deprecated methods for Strings (using only the first character). Could you find out where these are used and remove the call to those and use the char instead. All tests are passed but we should add more... especially with regards to the multi line one... Time to hit the sack!
    Former-commit-id: 073a2f879ad0aa77b7cce53f22a5f5dd35afacfe

--- PZFileReader/src/test/java/net/sf/pzfilereader/utilities/UnitTestUtils.java
+++ PZFileReader/src/test/java/net/sf/pzfilereader/utilities/UnitTestUtils.java
@@ -6,46 +6,30 @@ package net.sf.pzfilereader.utilities;
  * @author Paul Zepernick
  */
 public class UnitTestUtils {
-    
+
     /**
      * Builds a delimited qualified string containing the elements passed in
      * 
      * @param elements
-     *             Elements To Include In Delimited String
+     *            Elements To Include In Delimited String
      * @param delimiter
      * @param qualifier
      * @return String
      */
-    public static String buildDelimString(final String[] elements, final char delimiter, final char qualifier){
+    public static String buildDelimString(final String[] elements, final char delimiter, final char qualifier) {
         final StringBuffer sb = new StringBuffer();
-        for (int i = 0; i < elements.length; i++){
-            if (i > 0){
+        for (int i = 0; i < elements.length; i++) {
+            if (i > 0) {
                 sb.append(delimiter);
             }
-            sb.append(qualifier).append(elements[i]).append(qualifier);
+            if (qualifier != 0) {
+                sb.append(qualifier);
+            }
+            sb.append(elements[i]);
+            if (qualifier != 0) {
+                sb.append(qualifier);
+            }
         }
-        
-        
         return sb.toString();
     }
-    
-    /** Builds a delimited string containing the elements passed in
-    * 
-    * @param elements
-    *             Elements To Include In Delimited String
-    * @param delimiter
-    * @return String
-    */
-   public static String buildDelimString(final String[] elements, final char delimiter){
-       final StringBuffer sb = new StringBuffer();
-       for (int i = 0; i < elements.length; i++){
-           if (i > 0) {
-               sb.append(delimiter);
-           }
-           sb.append(elements[i]);
-       }
-       
-       
-       return sb.toString();
-   }
 }
