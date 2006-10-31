f29c76faa5774cf781450c9dedeba61e3119c160
benoitx <benoitx@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-10-31 17:12:14 +0000

    Paul, I've added some basic tests for null, empty, ",,," kind of things. I've also had a go at a parser, the regular expression is a dead-end or will become **extremely** complex due to our special and whacky cases... The basic tests make quite a few things break in the current version.  I'll run a couple of speed tests to see where we're going...

---
+++ src/main/java/net/sf/pzfilereader/util/BXParser.java
@@ -0,0 +1,110 @@
+/**
+ * 
+ */
+package net.sf.pzfilereader.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author xhensevb
+ * 
+ */
+public class BXParser {
+    public static List splitLine(String line, final char delimiter, char qualifier) {
+        List list = new ArrayList();
+
+        if (delimiter == 0) {
+            list.add(line);
+            return list;
+        } else if (line == null) {
+            return list;
+        }
+
+        final String trimmedLine = line.trim();
+        int size = trimmedLine.length();
+
+        if (size == 0) {
+            list.add(null);
+            return list;
+        }
+
+        boolean insideQualifier = false;
+        char previousChar = 0;
+        int startBlock = 0;
+        int endBlock = 0;
+        boolean blockWasInQualifier = false;
+
+        final String doubleQualifier = String.valueOf(qualifier) + String.valueOf(qualifier);
+        for (int i = 0; i < size; i++) {
+
+            final char currentChar = trimmedLine.charAt(i);
+
+            if (currentChar != delimiter && currentChar != qualifier) {
+                previousChar = currentChar;
+                endBlock = i + 1;
+                continue;
+            }
+
+            if (currentChar == delimiter) {
+                // we've found the delimiter (eg ,)
+                if (!insideQualifier) {
+                    String trimmed = trimmedLine.substring(startBlock, endBlock > startBlock ? endBlock : startBlock + 1);
+                    if (!blockWasInQualifier) {
+                        trimmed = trimmed.trim();
+                    }
+
+                    trimmed = trimmed.replaceAll(doubleQualifier, String.valueOf(qualifier));
+
+                    if (trimmed.length() == 1 && trimmed.charAt(0) == delimiter) {
+                        list.add(null);
+                    } else if (trimmed.length() == 1 && trimmed.charAt(0) == qualifier) {
+                        list.add("");
+                    } else {
+                        list.add(trimmed);
+                    }
+                    blockWasInQualifier = false;
+                    startBlock = i + 1;
+                }
+            } else if (currentChar == qualifier) {
+                if (!insideQualifier && previousChar != qualifier) {
+                    if (previousChar == delimiter || previousChar == 0 || previousChar == ' ') {
+                        insideQualifier = true;
+                        startBlock = i + 1;
+                    } else {
+                        endBlock = i + 1;
+                    }
+                } else {
+                    insideQualifier = false;
+                    blockWasInQualifier = true;
+                    endBlock = i;
+                    // last column (e.g. finishes with ")
+                    if (i == size - 1) {
+                        list.add(trimmedLine.substring(startBlock, size - 1));
+                        startBlock = i + 1;
+                    }
+                }
+            }
+            // antepenultimateChar = previousChar;
+            previousChar = currentChar;
+        }
+
+        if (startBlock < size) {
+            String str = trimmedLine.substring(startBlock, size);
+            str = str.replaceAll(doubleQualifier, String.valueOf(qualifier));
+            if (blockWasInQualifier) {
+                if (str.charAt(str.length() - 1) == qualifier) {
+                    list.add(str.substring(0, str.length() - 1));
+                } else {
+                    list.add(str);
+                }
+            } else {
+                list.add(str.trim());
+            }
+        } else if (trimmedLine.charAt(size - 1) == delimiter) {
+            list.add(null);
+        }
+
+        return list;
+    }
+}
