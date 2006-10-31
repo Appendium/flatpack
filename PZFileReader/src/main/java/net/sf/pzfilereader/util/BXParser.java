9945699982e10d358f6f030d7ead6eb019d4bcc2
Benoit Xhenseval <benoitx@users.noreply.github.com>
2006-10-31 22:47:08 +0000

    Paul, some interesting new methods, the BX parser (sorry about the name) seems to be significantly faster than the current one when there are NO qualifiers, If there are qualifiers, it is reasonably faster. But the interesting bits start when I created a new method that uses a StringBuffer rather than using "chunks". The new method is flying when there are qualifiers all over the place... but is slower (albeit not slower than the current one) when there are no qualifier... and that is a bit of a mystery... anyway... getting too late... Have a look and let me know.
    Former-commit-id: 97d8203aa7f7cafd678a5a96ee3358b90c2d5156

--- PZFileReader/src/main/java/net/sf/pzfilereader/util/BXParser.java
+++ PZFileReader/src/main/java/net/sf/pzfilereader/util/BXParser.java
@@ -12,7 +12,11 @@ import java.util.List;
  */
 public class BXParser {
     public static List splitLine(String line, final char delimiter, char qualifier) {
-        List list = new ArrayList();
+        return splitLine(line, delimiter, qualifier, 10);
+    }
+
+    public static List splitLine(String line, final char delimiter, char qualifier, int initialSize) {
+        List list = new ArrayList(initialSize);
 
         if (delimiter == 0) {
             list.add(line);
@@ -39,7 +43,6 @@ public class BXParser {
         for (int i = 0; i < size; i++) {
 
             final char currentChar = trimmedLine.charAt(i);
-
             if (currentChar != delimiter && currentChar != qualifier) {
                 previousChar = currentChar;
                 endBlock = i + 1;
@@ -52,9 +55,8 @@ public class BXParser {
                     String trimmed = trimmedLine.substring(startBlock, endBlock > startBlock ? endBlock : startBlock + 1);
                     if (!blockWasInQualifier) {
                         trimmed = trimmed.trim();
+                        trimmed = trimmed.replaceAll(doubleQualifier, String.valueOf(qualifier));
                     }
-
-                    trimmed = trimmed.replaceAll(doubleQualifier, String.valueOf(qualifier));
 
                     if (trimmed.length() == 1 && (trimmed.charAt(0) == delimiter || trimmed.charAt(0) == qualifier)) {
                         list.add("");
@@ -83,7 +85,6 @@ public class BXParser {
                     }
                 }
             }
-            // antepenultimateChar = previousChar;
             previousChar = currentChar;
         }
 
@@ -99,6 +100,110 @@ public class BXParser {
             } else {
                 list.add(str.trim());
             }
+        } else if (trimmedLine.charAt(size - 1) == delimiter) {
+            list.add("");
+        }
+
+        return list;
+    }
+
+    public static List splitLineWithBuf(String line, final char delimiter, char qualifier, int initialSize) {
+        List list = new ArrayList(initialSize);
+
+        if (delimiter == 0) {
+            list.add(line);
+            return list;
+        } else if (line == null) {
+            return list;
+        }
+
+        final String trimmedLine = line.trim();
+        int size = trimmedLine.length();
+
+        if (size == 0) {
+            list.add("");
+            return list;
+        }
+
+        boolean insideQualifier = false;
+        char previousChar = 0;
+        boolean blockWasInQualifier = false;
+        StringBuffer buf = new StringBuffer(32);
+
+        // final String doubleQualifier = String.valueOf(qualifier) +
+        // String.valueOf(qualifier);
+        for (int i = 0; i < size; i++) {
+            final char currentChar = trimmedLine.charAt(i);
+            if (currentChar != delimiter && currentChar != qualifier) {
+                previousChar = currentChar;
+                if (' ' != currentChar || insideQualifier || buf.length() > 0) {
+                    buf.append(currentChar);
+                }
+                continue;
+            }
+
+            if (currentChar == delimiter) {
+                // we've found the delimiter (eg ,)
+                if (!insideQualifier) {
+                    // String trimmed = trimmedLine.substring(startBlock,
+                    // endBlock > startBlock ? endBlock : startBlock + 1);
+                    String trimmed = buf.toString();
+                    if (!blockWasInQualifier) {
+                        trimmed = trimmed.trim();
+                        // trimmed = trimmed.replaceAll(doubleQualifier,
+                        // String.valueOf(qualifier));
+                    }
+
+                    if (trimmed.length() == 1 && (trimmed.charAt(0) == delimiter || trimmed.charAt(0) == qualifier)) {
+                        list.add("");
+                    } else {
+                        list.add(trimmed);
+                    }
+                    blockWasInQualifier = false;
+                    buf.delete(0, buf.length());
+                } else if (buf.length() != 1 || buf.charAt(0) != qualifier) {
+                    buf.append(currentChar);
+                } else {
+                    buf.delete(0, buf.length());
+                    insideQualifier = false;
+                    list.add("");
+                }
+            } else if (currentChar == qualifier) {
+                if (!insideQualifier && previousChar != qualifier) {
+                    if (previousChar == delimiter || previousChar == 0 || previousChar == ' ') {
+                        insideQualifier = true;
+                        int l = buf.length();
+                        if (l > 0) {
+                            buf.delete(0, l); // just entered a
+                            // qualifier, remove
+                            // whatever was
+                        }
+                    } else {
+                        buf.append(currentChar);
+                    }
+                } else {
+                    insideQualifier = false;
+                    blockWasInQualifier = true;
+                    if (previousChar == qualifier) {
+                        buf.append(qualifier);
+                        insideQualifier = true;
+                        previousChar = 0;
+                        continue;
+                    }
+                    // last column (e.g. finishes with ")
+                    if (i == size - 1) {
+                        // list.add(trimmedLine.substring(startBlock, size -
+                        // 1));
+                        list.add(buf.toString());
+                        buf.delete(0, buf.length());
+                    }
+                }
+            }
+            previousChar = currentChar;
+        }
+
+        if (buf.length() > 0) {
+            list.add(buf.toString().trim());
         } else if (trimmedLine.charAt(size - 1) == delimiter) {
             list.add("");
         }
