77c6bef16241007fee91cb94b8e7a4a4cbed7053
zepernick <zepernick@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-11-25 15:52:23 +0000

    - adding some new converter's - shored up some comments and added license agreement to files
    Former-commit-id: 521726ed484145f9f0472e0deccd2fc4305c22ac

--- PZFileReader/src/main/java/net/sf/pzfilereader/converter/ConvertInteger.java
+++ PZFileReader/src/main/java/net/sf/pzfilereader/converter/ConvertInteger.java
@@ -33,40 +33,24 @@
 
 package net.sf.pzfilereader.converter;
 
+import net.sf.pzfilereader.util.ParserUtils;
+
 /**
- * Exception thrown when a conversion error occurs
- * 
+ * Returns a Integer
+ * Non numeric chars are removed from the string
+ * before converting
+ *  
  * @author Paul Zepernick
  */
-public class PZConvertException extends RuntimeException{
-    private static final long serialVersionUID = 1L;
+public class ConvertInteger implements PZConverter {
 
-    /**
+    /*
+     * (non-Javadoc)
      * 
+     * @see net.sf.pzfilereader.converter#convertValue(java.lang.String)
      */
-    public PZConvertException() {
+    public Object convertValue(String valueToConvert) {
+        return new Integer(ParserUtils.stripNonLongChars(valueToConvert));
     }
 
-    /**
-     * @param message
-     */
-    public PZConvertException(final String message) {
-        super(message);
-    }
-
-    /**
-     * @param cause
-     */
-    public PZConvertException(final Throwable cause) {
-        super(cause);
-    }
-
-    /**
-     * @param message
-     * @param cause
-     */
-    public PZConvertException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-    
 }
