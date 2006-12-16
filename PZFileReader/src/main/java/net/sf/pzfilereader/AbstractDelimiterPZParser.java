25ba2bdb96bf4638d9fcea605f120aa687e2f8e7
zepernick <zepernick@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2006-12-16 17:57:34 +0000

    pbd fixes
    Former-commit-id: 73a470c57f2a7b867fdafd30f898770c466970fb

--- PZFileReader/src/main/java/net/sf/pzfilereader/AbstractDelimiterPZParser.java
+++ PZFileReader/src/main/java/net/sf/pzfilereader/AbstractDelimiterPZParser.java
@@ -34,7 +34,6 @@ package net.sf.pzfilereader;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
