b62bc1db88a4da7f088e229efb5bbce2450247b7
Benoit Xhenseval <benoitx@users.noreply.github.com>
2006-12-16 21:01:40 +0000

    checkstyle
    Former-commit-id: 1ca61d42b191ad9e4c503e9895998a29cc35b394

--- PZFileReader/src/main/java/net/sf/pzfilereader/util/ParserUtils.java
+++ PZFileReader/src/main/java/net/sf/pzfilereader/util/ParserUtils.java
@@ -39,7 +39,6 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.net.URL;
 import java.sql.Connection;
