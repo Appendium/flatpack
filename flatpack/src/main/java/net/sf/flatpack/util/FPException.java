5a549196881bc958e39e6dfc7051cce2fa26b329
zepernick <zepernick@509fa23a-e01f-0410-8d53-9f7e05c29f80>
2008-02-13 17:30:39 +0000

    prevent duplicate column names when using column names from file header
    Former-commit-id: b2117638dc117b5d55e1c5da86c89c3bdefb04fb

---
+++ flatpack/src/main/java/net/sf/flatpack/util/FPException.java
@@ -0,0 +1,13 @@
+package net.sf.flatpack.util;
+
+/**
+ * Generic exception for FlatPack
+ *
+ * @author Paul Zepernick
+ */
+public class FPException extends RuntimeException{
+    
+    public FPException(final String msg) {
+        super(msg);
+    }
+}
