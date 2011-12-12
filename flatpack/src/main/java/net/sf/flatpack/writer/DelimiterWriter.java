ab3e6459c70812693e604f8391550acf6f5a850d
Benoit Xhenseval <benoitx@users.noreply.github.com>
2011-12-12 23:31:41 +0000

    BigDecimal should not use the engineering/E-08 formats but simple plain strings.
    Former-commit-id: 85c50d9b02813cf03c7ca6c5a22fdad7c9d01972

--- flatpack/src/main/java/net/sf/flatpack/writer/DelimiterWriter.java
+++ flatpack/src/main/java/net/sf/flatpack/writer/DelimiterWriter.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.math.BigDecimal;
 
 import net.sf.flatpack.structure.ColumnMetaData;
 import net.sf.flatpack.util.FPConstants;
@@ -14,107 +15,114 @@ import net.sf.flatpack.util.FPConstants;
  * @author Dirk Holmes and Holger Holger Hoffstatte
  */
 public class DelimiterWriter extends AbstractWriter {
-    private char delimiter;
-    private char qualifier;
-    private List columnTitles = null;
-    private boolean columnTitlesWritten = false;
+	private char delimiter;
+	private char qualifier;
+	private List columnTitles = null;
+	private boolean columnTitlesWritten = false;
 
-    protected DelimiterWriter(final Map columnMapping, final java.io.Writer output, final char delimiter, final char qualifier) throws IOException {
-        super(output);
-        this.delimiter = delimiter;
-        this.qualifier = qualifier;
+	protected DelimiterWriter(final Map columnMapping,
+			final java.io.Writer output, final char delimiter,
+			final char qualifier) throws IOException {
+		super(output);
+		this.delimiter = delimiter;
+		this.qualifier = qualifier;
 
-        columnTitles = new ArrayList();
-        final List columns = (List) columnMapping.get(FPConstants.DETAIL_ID);
-        final Iterator columnIter = columns.iterator();
-        while (columnIter.hasNext()) {
-            final ColumnMetaData element = (ColumnMetaData) columnIter.next();
-            columnTitles.add(element.getColName());
-        }
-        // write the column headers
-        this.nextRecord();
-    }
+		columnTitles = new ArrayList();
+		final List columns = (List) columnMapping.get(FPConstants.DETAIL_ID);
+		final Iterator columnIter = columns.iterator();
+		while (columnIter.hasNext()) {
+			final ColumnMetaData element = (ColumnMetaData) columnIter.next();
+			columnTitles.add(element.getColName());
+		}
+		// write the column headers
+		this.nextRecord();
+	}
 
-    protected void writeWithDelimiter(final Object value) throws IOException {
-        this.write(value);
-        this.write(delimiter);
-    }
+	protected void writeWithDelimiter(final Object value) throws IOException {
+		this.write(value);
+		this.write(delimiter);
+	}
 
-    protected void write(final Object value) throws IOException {
-        String stringValue = "";
+	protected void write(final Object value) throws IOException {
+		String stringValue = "";
 
-        if (value != null) {
-            // TODO DO: format the value
-            stringValue = value.toString();
-        }
+		if (value != null) {
+			// TODO DO: format the value
+			if (value instanceof BigDecimal) {
+				final BigDecimal bd = (BigDecimal) value;
+				stringValue = bd.signum() == 0 ? "0" : bd.toPlainString();
+			} else {
+				stringValue = value.toString();
+			}
+		}
 
-        final boolean needsQuoting = (stringValue.indexOf(delimiter) != -1);
+		final boolean needsQuoting = (stringValue.indexOf(delimiter) != -1);
 
-        if (needsQuoting) {
-            super.write(qualifier);
-        }
+		if (needsQuoting) {
+			super.write(qualifier);
+		}
 
-        super.write(stringValue);
+		super.write(stringValue);
 
-        if (needsQuoting) {
-            super.write(qualifier);
-        }
-    }
+		if (needsQuoting) {
+			super.write(qualifier);
+		}
+	}
 
-    protected void addColumnTitle(final String string) {
-        if (string == null) {
-            throw new IllegalArgumentException("column title may not be null");
-        }
-        columnTitles.add(string);
-    }
+	protected void addColumnTitle(final String string) {
+		if (string == null) {
+			throw new IllegalArgumentException("column title may not be null");
+		}
+		columnTitles.add(string);
+	}
 
-    protected void writeColumnTitles() throws IOException {
-        final Iterator titleIter = columnTitles.iterator();
-        while (titleIter.hasNext()) {
-            final String title = (String) titleIter.next();
+	protected void writeColumnTitles() throws IOException {
+		final Iterator titleIter = columnTitles.iterator();
+		while (titleIter.hasNext()) {
+			final String title = (String) titleIter.next();
 
-            if (titleIter.hasNext()) {
-                this.writeWithDelimiter(title);
-            } else {
-                this.write(title);
-            }
-        }
-    }
+			if (titleIter.hasNext()) {
+				this.writeWithDelimiter(title);
+			} else {
+				this.write(title);
+			}
+		}
+	}
 
-    protected void writeRow() throws IOException {
-        final Iterator titlesIter = columnTitles.iterator();
-        while (titlesIter.hasNext()) {
-            final String columnTitle = (String) titlesIter.next();
-            if (getRowMap() != null) {
-                if (titlesIter.hasNext()) {
-                    writeWithDelimiter(getRowMap().get(columnTitle));
-                } else {
-                    write(getRowMap().get(columnTitle));
-                }
-            }
-        }
-    }
+	protected void writeRow() throws IOException {
+		final Iterator titlesIter = columnTitles.iterator();
+		while (titlesIter.hasNext()) {
+			final String columnTitle = (String) titlesIter.next();
+			if (getRowMap() != null) {
+				if (titlesIter.hasNext()) {
+					writeWithDelimiter(getRowMap().get(columnTitle));
+				} else {
+					write(getRowMap().get(columnTitle));
+				}
+			}
+		}
+	}
 
-    public final void nextRecord() throws IOException {
-        if (!columnTitlesWritten) {
-            this.writeColumnTitles();
-            columnTitlesWritten = true;
-        } else {
-            this.writeRow();
-        }
+	public final void nextRecord() throws IOException {
+		if (!columnTitlesWritten) {
+			this.writeColumnTitles();
+			columnTitlesWritten = true;
+		} else {
+			this.writeRow();
+		}
 
-        super.nextRecord();
-    }
+		super.nextRecord();
+	}
 
-    public void printFooter() {
-        // TODO DO: implement footer handling
-    }
+	public void printFooter() {
+		// TODO DO: implement footer handling
+	}
 
-    public void printHeader() {
-        // TODO DO: implement header handling
-    }
+	public void printHeader() {
+		// TODO DO: implement header handling
+	}
 
-    protected boolean validateColumnTitle(final String columnTitle) {
-        return columnTitles.contains(columnTitle);
-    }
+	protected boolean validateColumnTitle(final String columnTitle) {
+		return columnTitles.contains(columnTitle);
+	}
 }
