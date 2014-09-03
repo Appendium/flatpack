52ddf03d3fe00ace5fd667fde91c248d5911fb37
Benoit Xhenseval <benoitx@users.noreply.github.com>
2014-09-03 17:49:17 +0100

    Fix for #9

--- flatpack/src/main/java/net/sf/flatpack/DefaultDataSet.java
+++ flatpack/src/main/java/net/sf/flatpack/DefaultDataSet.java
@@ -102,6 +102,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getColumns()
      */
+    @Override
     public String[] getColumns() {
         String[] array = null;
 
@@ -122,6 +123,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getColumns(java.lang.String)
      */
+    @Override
     public String[] getColumns(final String recordID) {
         String[] array = null;
 
@@ -129,7 +131,7 @@ public class DefaultDataSet implements DataSet {
             final List<ColumnMetaData> cmds = ParserUtils.getColumnMetaData(recordID, metaData);
             array = new String[cmds.size()];
             for (int i = 0; i < cmds.size(); i++) {
-                final ColumnMetaData column = (ColumnMetaData) cmds.get(i);
+                final ColumnMetaData column = cmds.get(i);
                 array[i] = column.getColName();
             }
         }
@@ -137,22 +139,27 @@ public class DefaultDataSet implements DataSet {
         return array;
     }
 
+    @Override
     public Date getDate(final String column) throws ParseException {
         return currentRecord.getDate(column);
     }
 
+    @Override
     public Date getDate(final String column, final SimpleDateFormat sdf) throws ParseException {
         return currentRecord.getDate(column, sdf);
     }
 
+    @Override
     public double getDouble(final String column) {
         return currentRecord.getDouble(column);
     }
 
+    @Override
     public BigDecimal getBigDecimal(final String column) {
         return currentRecord.getBigDecimal(column);
     }
 
+    @Override
     public Object getObject(final String column, final Class<?> classToConvertTo) {
         return currentRecord.getObject(column, classToConvertTo);
     }
@@ -162,6 +169,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getErrorCount()
      */
+    @Override
     public int getErrorCount() {
         if (getErrors() != null) {
             return getErrors().size();
@@ -175,6 +183,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getErrors()
      */
+    @Override
     public List getErrors() {
         return errors;
     }
@@ -184,14 +193,17 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getIndex()
      */
+    @Override
     public int getIndex() {
         return pointer;
     }
 
+    @Override
     public int getInt(final String column) {
         return currentRecord.getInt(column);
     }
 
+    @Override
     public long getLong(final String column) {
         return currentRecord.getLong(column);
     }
@@ -201,6 +213,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getRowCount()
      */
+    @Override
     public int getRowCount() {
         return rows.size();
     }
@@ -210,6 +223,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getRowNo()
      */
+    @Override
     public int getRowNo() {
         return currentRecord.getRowNo();
     }
@@ -219,12 +233,14 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#getString(java.lang.String)
      */
+    @Override
     public String getString(final String column) {
         return currentRecord.getString(column);
     }
 
+    @Override
     public void setValue(final String column, final String value) {
-        final Row row = (Row) rows.get(pointer);
+        final Row row = rows.get(pointer);
         final int colIndex = ParserUtils.getColumnIndex(row.getMdkey(), metaData, column, parser.isColumnNamesCaseSensitive());
 
         row.setValue(colIndex, value);
@@ -235,6 +251,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#goBottom()
      */
+    @Override
     public void goBottom() {
         pointer = rows.size() - 1;
     }
@@ -244,6 +261,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#goTop()
      */
+    @Override
     public void goTop() {
         pointer = -1;
     }
@@ -253,9 +271,10 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#isAnError(int)
      */
+    @Override
     public boolean isAnError(final int lineNo) {
         for (int i = 0; i < errors.size(); i++) {
-            if (((DataError) errors.get(i)).getLineNo() == lineNo && ((DataError) errors.get(i)).getErrorLevel() > 1) {
+            if (errors.get(i).getLineNo() == lineNo && errors.get(i).getErrorLevel() > 1) {
                 return true;
             }
         }
@@ -267,10 +286,11 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#next()
      */
+    @Override
     public boolean next() {
         if (pointer < rows.size() && pointer + 1 != rows.size()) {
             pointer++;
-            currentRecord = new RowRecord((Row) rows.get(pointer), metaData, parser.isColumnNamesCaseSensitive(), pzConvertProps, strictNumericParse,
+            currentRecord = new RowRecord(rows.get(pointer), metaData, parser.isColumnNamesCaseSensitive(), pzConvertProps, strictNumericParse,
                     upperCase, lowerCase, parser.isNullEmptyStrings());
             return true;
         }
@@ -278,6 +298,7 @@ public class DefaultDataSet implements DataSet {
         return false;
     }
 
+    @Override
     public Record getRecord() {
         return currentRecord;
     }
@@ -287,6 +308,7 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#orderRows(net.sf.flatpack.ordering.OrderBy)
      */
+    @Override
     public void orderRows(final OrderBy ob) {
         // PZ try to handle other <records> by sending them to
         // the bottom of the sort
@@ -311,13 +333,14 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#previous()
      */
+    @Override
     public boolean previous() {
         if (pointer <= 0) {
             currentRecord = null;
             return false;
         }
         pointer--;
-        currentRecord = new RowRecord((Row) rows.get(pointer), metaData, parser.isColumnNamesCaseSensitive(), pzConvertProps, strictNumericParse,
+        currentRecord = new RowRecord(rows.get(pointer), metaData, parser.isColumnNamesCaseSensitive(), pzConvertProps, strictNumericParse,
                 upperCase, lowerCase, parser.isNullEmptyStrings());
         return true;
     }
@@ -325,6 +348,7 @@ public class DefaultDataSet implements DataSet {
     /**
      * Sets data in the DataSet to lowercase
      */
+    @Override
     public void setLowerCase() {
         upperCase = false;
         lowerCase = true;
@@ -333,6 +357,7 @@ public class DefaultDataSet implements DataSet {
     /**
      * Sets data in the DataSet to uppercase
      */
+    @Override
     public void setUpperCase() {
         upperCase = true;
         lowerCase = false;
@@ -344,6 +369,7 @@ public class DefaultDataSet implements DataSet {
      * @param recordID
      * @return boolean
      */
+    @Override
     public boolean isRecordID(final String recordID) {
         return currentRecord.isRecordID(recordID);
     }
@@ -355,12 +381,15 @@ public class DefaultDataSet implements DataSet {
      *            - int
      * @exception IndexOutOfBoundsException
      */
+    @Override
     public void absolute(final int localPointer) {
-        if (localPointer < 0 || localPointer > rows.size() - 1) {
+        if (localPointer < 0 || localPointer >= rows.size()) {
             throw new IndexOutOfBoundsException("INVALID POINTER LOCATION: " + localPointer);
         }
 
         pointer = localPointer;
+        currentRecord = new RowRecord(rows.get(pointer), metaData, parser.isColumnNamesCaseSensitive(), pzConvertProps, strictNumericParse,
+                upperCase, lowerCase, parser.isNullEmptyStrings());
     }
 
     /**
@@ -373,6 +402,7 @@ public class DefaultDataSet implements DataSet {
      * @param strictNumericParse
      *            The strictNumericParse to set.
      */
+    @Override
     public void setStrictNumericParse(final boolean strictNumericParse) {
         this.strictNumericParse = strictNumericParse;
     }
@@ -382,11 +412,13 @@ public class DefaultDataSet implements DataSet {
      * 
      * @see net.sf.flatpack.DataSet#remove()
      */
+    @Override
     public void remove() {
         rows.remove(pointer);
         pointer--;
     }
 
+    @Override
     public void setPZConvertProps(final Properties props) {
         this.pzConvertProps = props;
     }
@@ -399,17 +431,20 @@ public class DefaultDataSet implements DataSet {
         this.pointer = pointer;
     }
 
+    @Override
     public void clearRows() {
         pointer = -1; // set the pointer back to -1 directly just in case this
                       // instance is a BuffReaderDataSet.
         rows.clear();
     }
 
+    @Override
     public void clearAll() {
         clearRows();
         clearErrors();
     }
 
+    @Override
     public void clearErrors() {
         errors.clear();
     }
@@ -422,6 +457,7 @@ public class DefaultDataSet implements DataSet {
         this.metaData = metaData;
     }
 
+    @Override
     public String toString() {
         final StringBuilder buf = new StringBuilder();
         buf.append("Errors:").append(errors.size()).append(NEW_LINE);
@@ -432,6 +468,7 @@ public class DefaultDataSet implements DataSet {
         return buf.toString();
     }
 
+    @Override
     public boolean contains(final String column) {
         if (pointer == -1) {
             throw new IndexOutOfBoundsException("dataset on invalid row. need to call next()");
@@ -446,6 +483,7 @@ public class DefaultDataSet implements DataSet {
      * @throws FPException
      *             if cursor is on an invalid row
      */
+    @Override
     public boolean isRowEmpty() {
         if (!parser.isFlagEmptyRows()) {
             // flag empty rows needs to be set for this functionality
@@ -457,7 +495,7 @@ public class DefaultDataSet implements DataSet {
             throw new FPException("Cursor on invalid row..  Make sure next() is called and returns true");
         }
 
-        return ((Row) rows.get(pointer)).isEmpty();
+        return rows.get(pointer).isEmpty();
     }
 
     /**
@@ -465,6 +503,7 @@ public class DefaultDataSet implements DataSet {
      * @throws FPException
      *             if cursor is on an invalid row
      */
+    @Override
     public String getRawData() {
         if (!parser.isStoreRawDataToDataSet()) {
             // option needs to be set for this functionality
@@ -476,6 +515,6 @@ public class DefaultDataSet implements DataSet {
             throw new FPException("Cursor on invalid row.. Make sure next() is called and returns true");
         }
 
-        return ((Row) rows.get(pointer)).getRawData();
+        return rows.get(pointer).getRawData();
     }
 }
