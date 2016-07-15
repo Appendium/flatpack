2c22c70d5e8ace949453727d610cc9d5429fd915
Benoit Xhenseval <benoitx@users.noreply.github.com>
2016-07-15 18:58:37 +0100

    New FlatPack Writer Util
    Helper to create ColumnMetaData and Mapping for Writer Factory.

---
+++ flatpack/src/main/java/net/sf/flatpack/util/FlatpackWriterUtil.java
@@ -0,0 +1,63 @@
+/**
+ *
+ */
+package net.sf.flatpack.util;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.sf.flatpack.structure.ColumnMetaData;
+
+/**
+ * Helper class to create ColumnMetaData and Mapping for the Writer Factory.
+ * @author Benoit Xhenseval
+ */
+public final class FlatpackWriterUtil {
+    private FlatpackWriterUtil() {
+    }
+
+    /**
+     * Creates a Mapping for a WriterFactory for the given list of columns.
+     * @param colsAsCsv comma-separated column names
+     * @return a map to be used in, for instance, DelimiterWriterFactory
+     */
+    public static Map<String, Object> buildParametersForColumns(final String colsAsCsv) {
+        final Map<String, Object> mapping = new HashMap<>();
+        mapping.put(FPConstants.DETAIL_ID, buildColumns(colsAsCsv));
+        return mapping;
+    }
+
+    /**
+     * Create a new list of ColumnMetaData based on a CSV list of column titles.
+     * @param colsAsCsv
+     * @return new list
+     */
+    public static List<ColumnMetaData> buildColumns(final String colsAsCsv) {
+        final List<ColumnMetaData> listCol = new ArrayList<>();
+        buildColumns(listCol, colsAsCsv);
+        return listCol;
+    }
+
+    public static void buildColumns(final List<ColumnMetaData> listCol, final String cols) {
+        if (cols != null) {
+            for (final String s : cols.split(",")) {
+                final ColumnMetaData cmd = new ColumnMetaData();
+                cmd.setColName(s);
+                listCol.add(cmd);
+            }
+        }
+    }
+
+    public static void buildColumns(final List<ColumnMetaData> listCol, final String... cols) {
+        if (cols != null) {
+            for (final String s : cols) {
+                final ColumnMetaData cmd = new ColumnMetaData();
+                cmd.setColName(s);
+                listCol.add(cmd);
+            }
+        }
+    }
+
+}
